cb89ee82ceeaa27db187f7d754a8e4e2d1b21c99
parka25 <109412581+parka25@users.noreply.github.com>
2022-12-04 14:06:43 -0500

    Add NAV Tab into website, add an experiment html.

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
     const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+      user.title.toLowerCase().includes(value) ||
+      user.body.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://jsonplaceholder.typicode.com/posts")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
+      header.textContent = user.title
+      body.textContent = user.body
       userCardContainer.append(card)
       return { name: user.name, email: user.email, element: card }
     })
